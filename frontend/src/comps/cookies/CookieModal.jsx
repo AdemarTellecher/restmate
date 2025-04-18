e909bf18bca68934a97071c9128f16b3b71cc28b
aunjaffery <aunjaffery123@gmail.com>
2025-04-19 02:04:51 +0500

    cookie-done

--- frontend/src/comps/cookies/CookieModal.jsx
+++ frontend/src/comps/cookies/CookieModal.jsx
@@ -1,20 +1,21 @@
 import { LuCookie } from "react-icons/lu";
 import { useEffect, useState } from "react";
 import { useStore } from "../../store/store";
-import { dumbcookie } from "../../utils/utils";
 import ModalLayout from "../misc/ModalLayout";
 import CustomButton from "../misc/CustomButton";
 import CookieDetail from "./CookieDetail";
 import { ClearAllCookies, DeleteCookies, GetAllCookies } from "../../../wailsjs/go/main/App";
+import Spinner from "../misc/Spinner";
 
 const CookieModal = () => {
+  const [cookieLoad, setcookieLoad] = useState(true);
   const cookieModal = useStore((x) => x.cookieModal);
-  console.log("cookie modal --> ", cookieModal);
   const setCookieModal = useStore((x) => x.setCookieModal);
   const [cks, setCks] = useState(null);
   const getCooks = async () => {
+    setcookieLoad(true);
     let r = await GetAllCookies();
-    // let r = dumbcookie;
+    console.log(r);
     if (r.success && r.data && r.data.length) {
       let group = r?.data.reduce((acc, obj) => {
         const k = obj.Domain;
@@ -25,15 +26,17 @@ const CookieModal = () => {
         return acc;
       }, {});
       setCks(group);
-      // setCks(null);
+      setcookieLoad(false);
     } else {
       setCks(null);
+      setcookieLoad(false);
     }
   };
   useEffect(() => {
     getCooks();
   }, []);
   const deleteCookie = async (name) => {
+    setcookieLoad(false);
     let r = await DeleteCookies(name);
     if (r.success && r.data && r.data.length) {
       let group = r?.data.reduce((acc, obj) => {
@@ -45,20 +48,28 @@ const CookieModal = () => {
         return acc;
       }, {});
       setCks(group);
+      setcookieLoad(false);
     } else {
       setCks(null);
+      setcookieLoad(false);
     }
   };
   const clearCookie = async () => {
+    setcookieLoad(false);
     let r = await ClearAllCookies();
     if (r.success) {
       setCks(null);
     }
+    setcookieLoad(false);
   };
   return (
     <ModalLayout open={cookieModal} onClose={() => setCookieModal(false)} title="Cookies" size="xl">
       <div className="h-full p-4 overflow-y-auto" style={{ maxHeight: "800px", height: "400px" }}>
-        {cks && typeof cks === "object" && Object.keys(cks).length ? (
+        {cookieLoad ? (
+          <div className="h-full w-full flex justify-center items-center">
+            <Spinner />
+          </div>
+        ) : cks && typeof cks === "object" && Object.keys(cks).length ? (
           <div>
             <div className="flex items-center justify-end pb-2 pr-2">
               <div className="cursor-pointer" onClick={() => clearCookie()}>
